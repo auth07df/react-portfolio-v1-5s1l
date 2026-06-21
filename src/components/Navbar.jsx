4e0f782f65b53f71e5910359ceb367bcdf298131
auth07df <splurge-bust-polio@duck.com>
2026-06-22 02:21:14 +0530

    Add navigation component and basic styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Portfolio</h1>
+      <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
